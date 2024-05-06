4ee3ef5f91010caa376ed8990ea911b02633bec7
Arlene Huang <yanting.huang@columbia.edu>
2024-05-06 00:48:36 -0700

    Add image_normalize and arg_max helper functions

--- src/pickleml/pickleml.c
+++ src/pickleml/pickleml.c
@@ -26,12 +26,13 @@ float *read_file(const char *filename, size_t size) {
 
 // Y = W@X + b, W: weight = input_size * output_size, b: bias = output_size
 // X: input = input_size, Y: output = output_size
-void forward_linear_layer(float *output, float *input, float *weight,
-                          float *bias, size_t input_size, size_t output_size) {
+void forward_linear_layer(float *output, float *input,
+                          LinearLayer *linear_layer, size_t input_size,
+                          size_t output_size) {
   for (size_t i = 0; i < output_size; i++) {
-    output[i] = bias[i];
+    output[i] = linear_layer->bias[i];
     for (size_t j = 0; j < input_size; j++) {
-      output[i] += weight[j * output_size + i] * input[j];
+      output[i] += linear_layer->weight[i * input_size + j] * input[j];
     }
   }
 }
@@ -51,4 +52,23 @@ void forward_softmax(float *output, float *input, size_t size) {
   for (size_t i = 0; i < size; i++) {
     output[i] /= sum;
   }
+}
+
+void image_normalize(uint8_t *image, float *normalized_image, size_t size,
+                     float scale) {
+  for (size_t i = 0; i < size; i++) {
+    normalized_image[i] = ((image[i] / 255.0f) * 2.0f - 1.0f) * scale;
+  }
+}
+
+int arg_max(float *output, size_t size) {
+  int max_index = 0;
+  float max_value = output[0];
+  for (size_t i = 1; i < size; i++) {
+    if (output[i] > max_value) {
+      max_index = i;
+      max_value = output[i];
+    }
+  }
+  return max_index;
 }
--- src/pickleml/pickleml.h
+++ src/pickleml/pickleml.h
@@ -1,10 +1,24 @@
+#include <stdint.h>
 #include <stdlib.h>
+
+typedef struct {
+  float *weight;
+  float *bias;
+  int input_size;
+  int output_size;
+} LinearLayer;
 
 float *read_file(const char *filename, size_t size);
 
-void forward_linear_layer(float *output, float *input, float *weight,
-                          float *bias, size_t input_size, size_t output_size);
+void forward_linear_layer(float *output, float *input,
+                          LinearLayer *linear_layer, size_t input_size,
+                          size_t output_size);
 
 void forward_elu(float *output, float *input, size_t size);
 
 void forward_softmax(float *output, float *input, size_t size);
+
+void image_normalize(uint8_t *image, float *normalized_image, size_t size,
+                     float scale);
+
+int arg_max(float *output, size_t size);
