f29c9c35616f08f6ce0182f17b2b609d60083d1f
Arlene Huang <yanting.huang@columbia.edu>
2024-05-05 23:44:14 -0700

    Minor update on simpleFCN model

--- python/models.py
+++ python/models.py
@@ -1,16 +1,15 @@
 import torch
 
+
 class SimpleFCN(torch.nn.Module):
-    def __init__(self, input_size=(28,28), num_classes=10):
+    def __init__(self, input_size=(28, 28), num_classes=10):
         super().__init__()
         num_pixels = input_size[0] * input_size[1]
         self.net = torch.nn.Sequential(
             torch.nn.Linear(num_pixels, 256),
             torch.nn.ELU(),
-            torch.nn.Linear(256, 256),
-            torch.nn.ELU(),
             torch.nn.Linear(256, num_classes),
-            torch.nn.Softmax(dim=1)
+            torch.nn.Softmax(dim=1),
         )
 
     def forward(self, x):
